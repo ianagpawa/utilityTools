2e6f50b0ddde803375f706bd80b32f6cc79adeff
Ian Agpawa <agpawaji@gmail.com>
2020-04-05 23:28:01 -0400

    style: Update for utils

--- src/helperUtilities.js
+++ src/helperUtilities.js
@@ -1,21 +1,16 @@
 const fs = require('fs');
-function getEntryId(row) {
-    return getRowArr(row)[1];
-}
 
-function getRowArr(rowString) {
-    return rowString.split(',');
-}
+function getEntryId(row) { return getRowArr(row)[1]; }
+
+function getRowArr(rowString) { return rowString.split(','); }
+
+function dataFormatter(data) { return data.split(/\r?\n/); }
 
 function appendFile(fileName, row) {
     fs.appendFile(fileName, row, err => {
         if (err) throw err;
         console.log(`Update ${fileName} with ${row}`);
     })
-}
-
-function dataFormatter(data) {
-    return data.split(/\r?\n/);
 }
 
 module.exports = {
